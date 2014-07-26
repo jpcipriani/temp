4da5c99cd3610e1dea69948ff39d5285c3e5265d
Arthur Benemann <arthur.benemann@gmail.com>
2014-07-26 20:39:45 -0300

    Detectamos os contornos

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ while True:
 	i = i+1	
 	_, img = cam.read()
 
-	#img = cv2.resize(img,(100,100))
+	imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)	
+	cv2.imshow("imgray",imgray)
+	ret,thresh = cv2.threshold(imgray,127,255,0)
+	cv2.imshow("thresh",thresh)
+	contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-	cv2.imshow("lenna",img)
-	cv2.imwrite("%d.png"%i,img)
+	cv2.drawContours(img, contours, -1, (0,255,0), 3)
+	cv2.imshow("countours",img)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
         	break
