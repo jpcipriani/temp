3ee11806536cb0366bc1a0fea8b8340b36e326c5
Arthur Benemann <arthur.benemann@gmail.com>
2014-07-26 20:04:35 -0300

    Reduzimos a imagem

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import cv2
 print "Processamento de Imagem"
 
 img = cv2.imread("lenna.png")
-cv2.imshow("lenna",img)
 
-cv2.waitKey(3000)
+img = cv2.resize(img,(100,100))
+
+cv2.imshow("lenna",img)
+cv2.waitKey()
+
+cv2.imwrite("lenna.jpg",img)
+
