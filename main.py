08873e8695eb3e5ba9798920e95485041bd0e211
Arthur Benemann <arthur.benemann@gmail.com>
2014-07-26 21:44:33 -0300

    Tracbar adicionad

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
 
+def nothing(x):
+    pass
+
 print "Processamento de Imagem"
+cv2.namedWindow('image')
+cv2.createTrackbar("treshhold", "image", 127, 255, nothing) 
 
 cam = cv2.VideoCapture(0)
 i = 0
@@ -12,7 +17,7 @@ while True:
 
 	imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)	
 	cv2.imshow("imgray",imgray)
-	ret,thresh = cv2.threshold(imgray,127,255,0)
+	ret,thresh = cv2.threshold(imgray,cv2.getTrackbarPos("treshhold", "image"),255,0)
 	cv2.imshow("thresh",thresh)
 	contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
